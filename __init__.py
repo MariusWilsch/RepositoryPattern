ab9f8118d4fb46b3ceda4598fe7b5738d834bcac
mwilsch <marius.santiago.wilsch@gmail.com>
2024-07-30 13:51:08 +0200

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from .base_repository import (
+    BaseRepository,
+    SupabaseSingleton,
+    FilterOperator,
+    DatabaseError,
+)
+
+__all__ = ["BaseRepository", "SupabaseSingleton", "FilterOperator", "DatabaseError"]
